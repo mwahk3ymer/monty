33baad60e6aeb76adcffcbaac6f727985d0d50a4
Mwahk3ymer <audreymwakima99@gmail.com>
2023-08-24 16:38:14 +0000

    revert issues

--- opfun4.c
+++ opfun4.c
@@ -1,52 +1,6 @@
 #include "monty.h"
 
 /**
-<<<<<<< HEAD
- * rotl - s
- * @stack: Double linked list
- * @line_number: File line execution
- */
-void rotl(stack_t **stack, unsigned int line_number)
-{
-	stack_t *tm = *stack;
-=======
- * rotr - s
- * @stack: Double linked list
- * @line_number: File line execution
- */
-void rotr(stack_t **stack, unsigned int line_number)
-{
-	stack_t *tmp;
->>>>>>> 28493d18f292c3689e827d212d3088dd9a180b7e
-	(void) line_number;
-
-	if (!stack || !*stack || !(*stack)->next)
-		return;
-<<<<<<< HEAD
-	(*stack)->next->prev = NULL;
-	while (tm->next)
-		tm = tm->next;
-	tm->next = *stack;
-	(*stack) = (*stack)->next;
-	tm->next->next = NULL;
-	tm->next->prev = tm;
-=======
-
-	tmp = *stack;
-	while (tmp->next)
-		tmp = tmp->next;
-
-	tmp->next = *stack;
-	tmp->prev->next = NULL;
-	tmp->prev = NULL;
-
-	(*stack)->prev = tmp;
-	*stack = tmp;
-
->>>>>>> 28493d18f292c3689e827d212d3088dd9a180b7e
-}
-
-/**
  * stack - s
  * @stack: Double linked list
  * @line_number: File line execution
