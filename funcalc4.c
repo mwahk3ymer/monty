40b5cc1c67a225d1e91764f29329d0e166fd6b5f
Mwahk3ymer <audreymwakima99@gmail.com>
2023-08-25 04:37:09 -0700

    stack

--- funcalc4.c
+++ funcalc4.c
@@ -1,14 +1,14 @@
 #include "monty.h"
 
 /**
- * queue - so it does
+ * stack -  o it doess
  * @stack: Double linked list
  * @line_number: File line execution
  */
-void queue(stack_t **stack, unsigned int line_number)
+void stack(stack_t **stack, unsigned int line_number)
 {
 	(void) line_number;
 	(void) stack;
 
-	var.MODE = 1;
+	var.MODE = 0;
 }
